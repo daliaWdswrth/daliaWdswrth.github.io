baeb9ea658caebb447ffb78de1a3f4b25dd38b96
dlw <daliawor@gmail.com>
2023-10-16 15:55:12 -0400

    Updating images and icons for nav bar

---
+++ script.js
@@ -0,0 +1,7 @@
+// Include the navigation bar using JavaScript
+fetch("/nav_bar.html")
+        .then(response => response.text())
+        .then(data => {
+        document.body.insertAdjacentHTML('afterbegin', data);
+});
+
