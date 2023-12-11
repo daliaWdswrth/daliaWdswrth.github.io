42caf030dcbf280ad977a671b593d4bc9c26b69f
dlw <daliawor@gmail.com>
2023-12-11 15:57:09 -0500

    Redecorating

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 //placing nav bar on each page
-fetch("/nav_bar.html")
-        .then(response => response.text())
-        .then(data => {
-        document.body.insertAdjacentHTML('afterbegin', data);
-});
+// fetch("/nav_bar.html")
+//         .then(response => response.text())
+//         .then(data => {
+//         document.body.insertAdjacentHTML('afterbegin', data);
+// });
 
 //Function to organize calendar then track times for each event 
